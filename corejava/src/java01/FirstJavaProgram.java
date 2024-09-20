c7fe88d364e25936ffaccf582603d785e164f331
Harishwaran <harishwaran.mail@gmail.com>
2024-09-20 19:33:32 +0530

    adding this into repo

---
+++ corejava/src/java01/FirstJavaProgram.java
@@ -0,0 +1,7 @@
+package java01;
+
+public class FirstJavaProgram {
+	public static void main(String args[]) {
+		System.out.println("This is my FirstProgram I am welcoming everyone");
+	}
+}
---
+++ corejava/src/java02/Variables.java
@@ -0,0 +1,10 @@
+package java02;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
